217f25c9048d96865a7e6f809b81acd470f0e9cb
johnywojtek <johnywojtek@gmail.com>
2020-07-24 15:58:12 +0200

    Updates

---
+++ precache-manifest.a0ada07b57a105e2be7283c31bbf8020.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7db31b54f4bf16267ba7e46f313038da",
+    "url": "/ProductivitySystem/index.html"
+  },
+  {
+    "revision": "24313470c67796ef90d7",
+    "url": "/ProductivitySystem/static/css/10.e8c91ee4.chunk.css"
+  },
+  {
+    "revision": "bd8e726340f6c26b2d84",
+    "url": "/ProductivitySystem/static/css/11.54f053ef.chunk.css"
+  },
+  {
+    "revision": "cb628e6858c43dbc97ad",
+    "url": "/ProductivitySystem/static/css/15.455aba90.chunk.css"
+  },
+  {
+    "revision": "628118f8cb6f87c65b6f",
+    "url": "/ProductivitySystem/static/css/6.b93734cd.chunk.css"
+  },
+  {
+    "revision": "85d345655be007421b35",
+    "url": "/ProductivitySystem/static/js/0.0e269e20.chunk.js"
+  },
+  {
+    "revision": "b325e65166b7c305a3cb",
+    "url": "/ProductivitySystem/static/js/1.acaa2228.chunk.js"
+  },
+  {
+    "revision": "24313470c67796ef90d7",
+    "url": "/ProductivitySystem/static/js/10.04363487.chunk.js"
+  },
+  {
+    "revision": "bd8e726340f6c26b2d84",
+    "url": "/ProductivitySystem/static/js/11.70986513.chunk.js"
+  },
+  {
+    "revision": "c54f4dfdc2090630b237",
+    "url": "/ProductivitySystem/static/js/12.04d0cb71.chunk.js"
+  },
+  {
+    "revision": "c42f35f80ec2ca425277",
+    "url": "/ProductivitySystem/static/js/13.ce542fe2.chunk.js"
+  },
+  {
+    "revision": "15ecdb235532e9cc53ec",
+    "url": "/ProductivitySystem/static/js/14.0629fa94.chunk.js"
+  },
+  {
+    "revision": "cb628e6858c43dbc97ad",
+    "url": "/ProductivitySystem/static/js/15.642e8a42.chunk.js"
+  },
+  {
+    "revision": "c025547b1b4f284452e1",
+    "url": "/ProductivitySystem/static/js/16.9e0c0c51.chunk.js"
+  },
+  {
+    "revision": "417a65935515ff7f4539",
+    "url": "/ProductivitySystem/static/js/17.73d20856.chunk.js"
+  },
+  {
+    "revision": "1b711071c236dd4297a8",
+    "url": "/ProductivitySystem/static/js/18.daa851e8.chunk.js"
+  },
+  {
+    "revision": "f7a4c70299a6a7a55249",
+    "url": "/ProductivitySystem/static/js/19.cefd321d.chunk.js"
+  },
+  {
+    "revision": "9432b9646b6493073595",
+    "url": "/ProductivitySystem/static/js/2.1ba7b12e.chunk.js"
+  },
+  {
+    "revision": "3eba676009d6590c5e95",
+    "url": "/ProductivitySystem/static/js/20.fd289eac.chunk.js"
+  },
+  {
+    "revision": "5c3e3c6c8e45904dae2e",
+    "url": "/ProductivitySystem/static/js/21.6b938e12.chunk.js"
+  },
+  {
+    "revision": "4d7ed7cbea04ac897769",
+    "url": "/ProductivitySystem/static/js/22.48bc1f37.chunk.js"
+  },
+  {
+    "revision": "428a4b3a461a2149be1e",
+    "url": "/ProductivitySystem/static/js/23.de5c74a4.chunk.js"
+  },
+  {
+    "revision": "dcbb3a0002ebecfa41e6",
+    "url": "/ProductivitySystem/static/js/24.6c2538b0.chunk.js"
+  },
+  {
+    "revision": "8c2d0d36f3ba9c5dd711",
+    "url": "/ProductivitySystem/static/js/25.13fe794c.chunk.js"
+  },
+  {
+    "revision": "0e3f46b1c67797cc226c",
+    "url": "/ProductivitySystem/static/js/26.3a643aaa.chunk.js"
+  },
+  {
+    "revision": "5edc8e89bf655e39095c",
+    "url": "/ProductivitySystem/static/js/27.62a53254.chunk.js"
+  },
+  {
+    "revision": "2a5eb700cc859866388e",
+    "url": "/ProductivitySystem/static/js/28.6bb28eef.chunk.js"
+  },
+  {
+    "revision": "16c18bcb14a05ddaf775",
+    "url": "/ProductivitySystem/static/js/29.29147eed.chunk.js"
+  },
+  {
+    "revision": "95b509af803dc1e7307d",
+    "url": "/ProductivitySystem/static/js/3.0c580556.chunk.js"
+  },
+  {
+    "revision": "55ada92fefb791d1e7ea",
+    "url": "/ProductivitySystem/static/js/30.f9581e9e.chunk.js"
+  },
+  {
+    "revision": "0cd6aad165ab55bf20fa",
+    "url": "/ProductivitySystem/static/js/31.0f609de9.chunk.js"
+  },
+  {
+    "revision": "638cf46b30c5eec55718",
+    "url": "/ProductivitySystem/static/js/32.16238062.chunk.js"
+  },
+  {
+    "revision": "8d4d419686b68059bf65",
+    "url": "/ProductivitySystem/static/js/33.e7d70068.chunk.js"
+  },
+  {
+    "revision": "4d6c3f01aebb2d476003",
+    "url": "/ProductivitySystem/static/js/34.c7ba6b18.chunk.js"
+  },
+  {
+    "revision": "7e40e250044d892b9107",
+    "url": "/ProductivitySystem/static/js/35.a5191d2f.chunk.js"
+  },
+  {
+    "revision": "ade0f65001c12dc416bb",
+    "url": "/ProductivitySystem/static/js/36.617a77e6.chunk.js"
+  },
+  {
+    "revision": "25f7c969594baa16181f",
+    "url": "/ProductivitySystem/static/js/37.2e759380.chunk.js"
+  },
+  {
+    "revision": "52ea19fb18f7bd50c130",
+    "url": "/ProductivitySystem/static/js/38.176124b1.chunk.js"
+  },
+  {
+    "revision": "1e40d0687e33ebda21a1",
+    "url": "/ProductivitySystem/static/js/39.4d039073.chunk.js"
+  },
+  {
+    "revision": "78de1c57455a354bf5b6",
+    "url": "/ProductivitySystem/static/js/4.23184501.chunk.js"
+  },
+  {
+    "revision": "95ff4a5d657d2d84028f",
+    "url": "/ProductivitySystem/static/js/5.c6adebea.chunk.js"
+  },
+  {
+    "revision": "628118f8cb6f87c65b6f",
+    "url": "/ProductivitySystem/static/js/6.be5bf3c9.chunk.js"
+  },
+  {
+    "revision": "7e508b5a61359e6b377c",
+    "url": "/ProductivitySystem/static/js/7.7754c7ec.chunk.js"
+  },
+  {
+    "revision": "8b46b3229e245ecbae3f",
+    "url": "/ProductivitySystem/static/js/main.b99c8e65.chunk.js"
+  },
+  {
+    "revision": "34bf841b42943b8c1305",
+    "url": "/ProductivitySystem/static/js/runtime~main.e8702992.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ProductivitySystem/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ProductivitySystem/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ProductivitySystem/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ProductivitySystem/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ProductivitySystem/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
